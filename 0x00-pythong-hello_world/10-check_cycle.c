63f28a992745ad6a83cfdaf7961cfaa01aa17682
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-04 16:59:34 -0800

    Added in all files

---
+++ 0x00-pythong-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list, *slow = list;
+
+	while (fast->next && slow->next)
+	{
+		if (fast->next->next)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+		}
+		else
+			break;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+}
