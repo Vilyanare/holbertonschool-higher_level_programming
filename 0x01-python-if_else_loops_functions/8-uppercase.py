1819198b2e771af3e9cb7f85a290db2db09766bc
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-05 17:48:14 -0800

    8 no longer uses counter

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    count = 0
     for c in str:
         a = ord(c)
-        count += 1
         if ord(c) > 96 and ord(c) < 123:
             a -= 32
         print("{:c}".format(a), end='')
-        if count == len(str):
-            print("")
+    print("")
