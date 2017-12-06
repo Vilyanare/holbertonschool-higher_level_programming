ae10368be823fb4d18a2aebe1af9e41c5a6a5b99
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-05 17:44:09 -0800

    Changed 8 completely

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,10 @@
 def uppercase(str):
     count = 0
     for c in str:
+        a = ord(c)
         count += 1
         if ord(c) > 96 and ord(c) < 123:
-            print("{:c}".format(ord(c) - 32), end='')
-        else:
-            print("{:c}".format(ord(c)), end="")
+            a -= 32
+        print("{:c}".format(a), end='')
         if count == len(str):
             print("")
