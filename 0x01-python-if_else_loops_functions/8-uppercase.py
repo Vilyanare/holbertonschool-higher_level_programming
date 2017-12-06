9ba3f644bdadb469e09a66355fcfc579e993439e
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-05 17:30:39 -0800

    Changed to string format style

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,8 @@ def uppercase(str):
     for c in str:
         count += 1
         if ord(c) > 96 and ord(c) < 123:
-            print(chr(ord(c) - 32), end='')
+            print("{:c}".format(ord(c) - 32), end='')
         else:
-            print(c, end="")
+            print("{:c}".format(ord(c)), end="")
         if count == len(str):
             print("")
