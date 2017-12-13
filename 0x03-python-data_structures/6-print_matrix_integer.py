88f1415eca1cf15fcc87512147fd8f1f29fb2d81
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-12 19:27:12 -0800

    Added a null check to 6.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=' ')
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                    print("{:d}".format(matrix[i][j]), end='')
+                    if j != len(matrix[row]) - 1:
+                        print("{}".format(" "), end='')
+            print()
