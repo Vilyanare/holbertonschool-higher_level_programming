014b7a941d7a5fa39d115f09f58f915363a474c5
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-13 12:00:27 -0800

    Updated 13 to run faster

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,72 @@
 #include "lists.h"
 /**
+ * free_listintrev - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listintrev(listintrev_t *head)
+{
+    listintrev_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
+/**
+ * revlist - reverse a listint_t list
+ * @head: head of listint_t list
+ * Return: address to head of reversed list
+ */
+listintrev_t *revlist(listint_t *head)
+{
+	listintrev_t *rev = NULL, *temp = NULL;
+
+	while (head)
+	{
+		temp = malloc(sizeof(listintrev_t));
+		if (temp == NULL)
+		{
+			if (rev)
+				free_listintrev(rev);
+			return (NULL);
+		}
+		temp->n = head->n;
+		temp->oadd = head;
+		if (rev)
+			temp->next = rev;
+		else
+			temp->next = NULL;
+		rev = temp;
+		head = head->next;
+	}
+	return (rev);
+}
+/**
  * is_palindrome - check to see if data in a linked list is a palindrome
  * @head: head of linked list
  * Return: 0 if not 1 if is palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *front = NULL, *back = NULL, *backback = NULL;
+	listintrev_t *revfront = NULL;
+	listint_t *front = NULL;
 
 	if (head == NULL)
 		return (0);
 	if (*head == NULL)
 		return (1);
 	front = *head;
-	back = *head;
-	while (back->next)
-		back = back->next;
-	backback = back;
-	if (back->n == front->n)
+	revfront = revlist(*head);
+	while (revfront->n == front->n)
 	{
-		while (1)
-		{
-			if (front == back || front->next == back)
-				return (1);
-			if (front->n == back->n)
-			{
-				front = front->next;
-				back = front;
-				while(back->next != backback)
-					back = back->next;
-				backback = back;
-			}
-			else
-				break;
-		}
+		if (revfront->oadd == front || front->next == revfront->oadd)
+			return (1);
+		revfront = revfront->next;
+		front = front->next;
+
 	}
 	return (0);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -15,11 +15,26 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+/**
+ * struct listintrev_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * @oadd: address of node in non reversed list
+ *
+ * Description: reversed list holding address of lisint_t node
+ */
+typedef struct listintrev_s
+{
+    int n;
+    struct listintrev_s *next;
+    struct listint_s *oadd;
+} listintrev_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+void free_listintrev(listintrev_t *head);
+listintrev_t *revlist(listint_t *head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
