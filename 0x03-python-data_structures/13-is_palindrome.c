497fbfe129613aacb6dc4d0645fe97223e445bac
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-12-12 20:07:32 -0800

    for 13 if null palindrome is yes

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,33 +8,29 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *front = NULL, *back = NULL, *backback = NULL;
 
-	if (head)
+	if (!(head || *head))
+		return (1);
+	front = *head;
+	back = *head;
+	while (back->next)
+		back = back->next;
+	backback = back;
+	if (back->n == front->n)
 	{
-		if (*head)
+		while (1)
 		{
-			front = *head;
-			back = *head;
-			while (back->next)
-				back = back->next;
-			backback = back;
-			if (back->n == front->n)
+			if (front == back || front->next == back)
+				return (1);
+			if (front->n == back->n)
 			{
-				while (1)
-				{
-					if (front == back || front->next == back)
-						return (1);
-					if (front->n == back->n)
-					{
-						front = front->next;
-						back = front;
-						while(back->next != backback)
-							back = back->next;
-						backback = back;
-					}
-					else
-						break;
-				}
+				front = front->next;
+				back = front;
+				while(back->next != backback)
+					back = back->next;
+				backback = back;
 			}
+			else
+				break;
 		}
 	}
 	return (0);
