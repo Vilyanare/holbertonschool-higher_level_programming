36108cc65a1ee3e71f66fbf6f3218a7d341d2771
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-01-02 18:58:27 -0800

    Changed 100 to use .format

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,4 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as inst:
-        sys.stderr.write("Exception: " + str(inst) + "\n")
+        sys.stderr.write("Exception: {}\n".format(inst))
+        return False
