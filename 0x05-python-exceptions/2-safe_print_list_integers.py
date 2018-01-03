6cd74bb5b55c1f1ae6847f18e619c45d9a500b0d
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-01-02 17:37:02 -0800

    Added typeerror handling to 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,9 @@ def safe_print_list_integers(my_lists=[], x=0):
         try:
             print("{:d}".format(my_lists[i]), end="")
             count += 1
-        except (ValueError):
+        except ValueError:
+            pass
+        except TypeError:
             pass
     print()
     return count
