aa69b8e2087e43cb20f080ec226cd533fefcd9d4
vilyanare <acolyteofwar@gmail.com>
2018-01-23 17:49:38 -0800

    changed error message for x and y value errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,7 +106,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError('x must be an integer')
         if value < 0:
-            raise ValueError('x must be >= to 0')
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -120,5 +120,5 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError('y must be an integer')
         if value < 0:
-            raise ValueError('y must be >= to 0')
+            raise ValueError('y must be >= 0')
         self.__y = value
