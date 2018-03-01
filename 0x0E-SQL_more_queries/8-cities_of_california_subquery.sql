be4aaaad6f58c817b62c8e199b67f3669b49dd25
vilyanare <acolyteofwar@gmail.com>
2018-02-28 16:07:39 -0800

    Adding in all files for 0x0E

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Lists all cities of California in hbtn_0d_usa
+SELECT id, name
+FROM hbtn_0d_usa.cities
+WHERE state_id =
+    (SELECT id
+    FROM hbtn_0d_usa.states
+    WHERE name = 'California')
