a38c51736b4ddc35673d80f5091cedacf9d64a3a
vilyanare <acolyteofwar@gmail.com>
2018-04-03 16:02:00 -0700

    Condenses list by removing duplicate adjacent values

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,13 +8,25 @@ def find_peak(list_of_integers):
     '''
     finds a peak number in a list of integers
     '''
-    new_list = list_of_integers
-    while True:
+    new_list = list_of_integers[:1]
+    for x in range(1, len(list_of_integers)):
+        if list_of_integers[x] != list_of_integers[x - 1]:
+            new_list.append(list_of_integers[x])
+
+    while new_list:
         half = int(len(new_list) / 2)
         pivot = new_list[half]
-        if new_list[half - 1] < pivot > new_list[half + 1]:
+        if half - 1 < 0:
+            lower = 0
+        else:
+            lower = new_list[half - 1]
+        if half + 1 > len(new_list) - 1:
+            upper = 0
+        else:
+            upper = new_list[half + 1]
+        if lower < pivot > upper:
             return pivot
-        if new_list[half + 1] > pivot:
+        if upper > pivot:
             new_list = new_list[half - 1:]
         else:
             new_list = new_list[:half]
