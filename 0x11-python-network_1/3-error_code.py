019c3bd225ebde1c6a399177a3b630abd0bb5f01
vilyanare <acolyteofwar@gmail.com>
2018-04-09 17:42:44 -0700

    Changed import so it looks like urllib.requests

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,10 +4,11 @@
 '''
 if __name__ == "__main__":
     from sys import argv
-    from urllib import request, error
-    req = request.Request(argv[1])
+    import urllib.request
+
+    req = urllib.request.Request(argv[1])
     try:
-        html = request.urlopen(req)
+        html = urllib.request.urlopen(req)
         print(html.read().decode('utf-8'))
-    except error.HTTPError as e:
+    except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
