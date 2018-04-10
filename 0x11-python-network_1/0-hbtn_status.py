41f7cc97415aaaa70cc6520452bd6c090a005f54
vilyanare <acolyteofwar@gmail.com>
2018-04-09 17:51:37 -0700

    Change spaces to tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
     print('''Body response:
-        - type:{}
-        - content:{}
-        - utf8 content:{}'''.format(type(html), html, html.decode('utf-8')))
+    \t- type: {}
+    \t- content: {}
+    \t- utf8 content: {}'''.format(type(html), html, html.decode('utf-8')))
