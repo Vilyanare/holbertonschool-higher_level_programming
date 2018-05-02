844715c0751912e31865509e28ba85a7ba564ef3
vilyanare <acolyteofwar@gmail.com>
2018-05-01 17:30:10 -0700

    Now finds second highest number instead of highest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,15 @@
 const lenArgs = process.argv.length;
 let i = 0;
 let max = -Infinity;
+let secondHighest = -Infinity;
 if (lenArgs < 4) {
   console.log(0);
 } else {
   for (i = 2; i < lenArgs; i++) {
     if (parseInt(process.argv[i]) > max) {
+      secondHighest = max;
       max = parseInt(process.argv[i]);
     }
   }
-  console.log(max);
+  console.log(secondHighest);
 }
