a76c6ad5f0d03f1bda1663e88612291e26074ac6
vilyanare <acolyteofwar@gmail.com>
2018-05-01 17:24:05 -0700

    Changed cool to fun in 0 index of array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is cool', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let i = 0;
 for (i = 0; i < 3; i++) {
   console.log(languages[i]);
