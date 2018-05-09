197da552dcfd52fd4017b0cc5f85be4dfda6da27
vilyanare <acolyteofwar@gmail.com>
2018-05-08 17:24:06 -0700

    Removed ./ from path

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Opens a file and prints what's inside
 const fs = require('fs');
-let filePath = './' + process.argv[2];
+let filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) console.log(err);
